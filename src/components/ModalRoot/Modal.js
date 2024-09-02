eeb6b3081705c4c0b47da819be3733c99c8f1f82
huusangcv <huusangcv@gmail.com>
2024-09-02 13:00:26 +0700

    add page developing

--- src/components/ModalRoot/Modal.js
+++ src/components/ModalRoot/Modal.js
@@ -3,7 +3,6 @@ import classNames from 'classnames/bind';
 import styles from './Modal.module.scss';
 const cx = classNames.bind(styles);
 const Modal = ({ isShowModalTrailer, onClickCloseTrailers, trailer }) => {
-  console.log({ trailer });
   return (
     <>
       {isShowModalTrailer && (
