45765841fe45f9cfa2e3e04c70542ffd40963344
huusangcv <huusangcv@gmail.com>
2024-11-13 21:08:38 +0700

    feat: update notfound page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,13 +1,56 @@
-import SimpleSlider from '~/components/SimpleSlider/SimpleSlider';
+const divStyle = {
+  color: '#000',
+  background: '#fff',
+  fontFamily:
+    '-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif',
+  height: '100vh',
+  textAlign: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
 
+const h1Style = {
+  display: 'inline-block',
+  borderRight: '1px solid rgba(0, 0, 0, .3)',
+  margin: 0,
+  marginRight: '20px',
+  padding: '10px 23px 10px 0',
+  fontSize: '24px',
+  fontWeight: 500,
+  verticalAlign: 'top',
+};
+
+const h2Style = {
+  fontSize: '14px',
+  fontWeight: 'normal',
+  lineHeight: 'inherit',
+  margin: 0,
+  padding: 0,
+};
 const NotFound = () => {
   return (
-    <div>
-      <h3 className="section-header">Phim tương tự</h3>
-      <div className="related-titles">
-        <SimpleSlider />
+    <section class="section">
+      <div class="container">
+        <div style={divStyle}>
+          <div>
+            <h1 style={h1Style}>404</h1>
+            <div
+              style={{
+                display: 'inline-block',
+                textAlign: 'left',
+                lineHeight: '49px',
+                height: '49px',
+                verticalAlign: 'middle',
+              }}
+            >
+              <h2 style={h2Style}>This page could not be found.</h2>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
