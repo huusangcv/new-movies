b53e7db5cfade1aa599f3a989e9a02e1af0bafad
huusangcv <huusangcv@gmail.com>
2024-12-31 19:19:02 +0700

    fix: update login page

--- src/pages/Auth/Login/Login.js
+++ src/pages/Auth/Login/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [setCookie] = useCookies(['token']);
+  const [cookies, setCookie] = useCookies(['token']);
   const navigate = useNavigate();
 
   useEffect(() => {
